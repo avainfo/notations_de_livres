631bad574e1f455a0bc6692e20edc4bf5102c6b6
anton <avainfoconseils@gmail.com>
2024-07-17 21:30:25 +0200

    refactor: changed routes declarations

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 const router = express.Router();
 
 const cors = require('cors');
+const routes = require("./utils/routes");
 
 connect();
 
@@ -16,41 +17,11 @@ app.use(cors())
 
 app.use(express.json());
 
-router.post('/auth/signup', async (req, res, next) => {
-	const email = req.body["email"];
-	let password = req.body["password"];
-
-	password = await hash(password);
-
-	const user = new User({email, password});
-	user.save()
-		.then((e) => res.status(200).json({'message': e}))
-		.catch((e) => res.status(401).json(e.errorResponse));
-});
-
-router.post('/auth/login', async (req, res, next) => {
-	const email = req.body["email"];
-	let password = await hash(req.body["password"]);
-
-	User.findOne({email: email})
-		.then(user => {
-			if (!user) {
-				res.status(401).json({error: 'Utilisateur non trouvé !'});
-			}
-			if (password !== user.password) {
-				res.status(401).json({error: 'Mot de passe incorrect !'});
-			} else {
-				res.status(200).json({
-					userId: user._id, token: sign({
-						userId: user._id
-					}, process.env.JWT_SECRET, {
-						expiresIn: '24h'
-					},)
-				});
-			}
-		})
-		.catch(error => res.status(500).json({error}));
-})
+for(let r of routes) {
+	if(r.method === "post") {
+		router.post(r.path, r.action())
+	}
+}
 
 app.use('/api', router);
 
---
+++ utils/routes.js
@@ -0,0 +1,50 @@
+const hash = require("./Hash");
+const User = require("../models/UserModel");
+const {sign} = require("jsonwebtoken");
+
+const routes = [
+	{method: "post", path: "/auth/signup", action: signup},
+	{method: "post", path: "/auth/login", action: login},
+]
+
+function signup() {
+	return async (req, res, next) => {
+		const email = req.body["email"];
+		let password = req.body["password"];
+
+		password = await hash(password);
+
+		const user = new User({email, password});
+		user.save()
+			.then((e) => res.status(200).json({'message': e}))
+			.catch((e) => res.status(401).json(e.errorResponse));
+	};
+}
+
+function login() {
+	return async (req, res, next) => {
+		const email = req.body["email"];
+		let password = await hash(req.body["password"]);
+
+		User.findOne({email: email})
+			.then(user => {
+				if (!user) {
+					res.status(401).json({error: 'Utilisateur non trouvé !'});
+				}
+				if (password !== user.password) {
+					res.status(401).json({error: 'Mot de passe incorrect !'});
+				} else {
+					res.status(200).json({
+						userId: user._id, token: sign({
+							userId: user._id
+						}, process.env.JWT_SECRET, {
+							expiresIn: '24h'
+						},)
+					});
+				}
+			})
+			.catch(error => res.status(500).json({error}));
+	};
+}
+
+module.exports = routes;
