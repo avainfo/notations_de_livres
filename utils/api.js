f050b15f6c2a7b182066ac4e916ae58b87b93c1c
anton <avainfoconseils@gmail.com>
2024-07-19 19:22:55 +0200

    fix: change connection with Atlas MongoDb

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-	await mongoose.connect("mongodb://127.0.0.1:27017/users")
+	await mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.gt24oyi.mongodb.net/users`)
 		.then((e) => console.log("Connecté à la base de données"))
 		.catch((e) => console.log(e));
 }
